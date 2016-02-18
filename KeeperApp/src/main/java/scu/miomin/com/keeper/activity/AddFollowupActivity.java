591b0c0856757e509d37f13c66fa3836f285feaf
miomin <691292118@qq.com>
2016-02-18 12:39:08 +0800

    修改AddFollowupActivity

--- KeeperApp/src/main/java/scu/miomin/com/keeper/activity/AddFollowupActivity.java
+++ KeeperApp/src/main/java/scu/miomin/com/keeper/activity/AddFollowupActivity.java
@@ -20,6 +20,7 @@ public class AddFollowupActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_followup);
+
     }
 
     public static void actionStart(Context context) {
